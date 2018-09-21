9f993730c1a826d82954ba91927b7062e2a6e327
Mariyam Fedoseeva <mfedosseyeva@gmail.com>
2018-09-22 00:07:18 +0200

    inscrease husky speed

--- Experiment/project_traffic_signs/husky_control.py
+++ Experiment/project_traffic_signs/husky_control.py
@@ -5,19 +5,20 @@ import geometry_msgs.msg
 @nrp.MapVariable("sign", initial_value=None, scope=nrp.GLOBAL)
 @nrp.Neuron2Robot(Topic('/husky/cmd_vel', geometry_msgs.msg.Twist))
 def velo_control(t, sign):
+	coeff = 5
 	if sign.value == 'limit100':
 		return geometry_msgs.msg.Twist(
-        linear=geometry_msgs.msg.Vector3(x=1.0, y=0.0, z=0.0),
+        linear=geometry_msgs.msg.Vector3(x=1.0*coeff, y=0.0, z=0.0),
         angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=0.0))
 	elif sign.value == 'limit20':
 		return geometry_msgs.msg.Twist(
-        linear=geometry_msgs.msg.Vector3(x=0.2, y=0.0, z=0.0),
+        linear=geometry_msgs.msg.Vector3(x=0.2*coeff, y=0.0, z=0.0),
         angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=0.0))
 	elif sign.value == 'stop_sign':
 		return geometry_msgs.msg.Twist(
-        linear=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=0.0),
+        linear=geometry_msgs.msg.Vector3(x=0.0*coeff, y=0.0, z=0.0),
         angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=0.0))
 	else:
 		return geometry_msgs.msg.Twist(
-        linear=geometry_msgs.msg.Vector3(x=0.6, y=0.0, z=0.0),
+        linear=geometry_msgs.msg.Vector3(x=0.6*coeff, y=0.0, z=0.0),
         angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=0.0))
